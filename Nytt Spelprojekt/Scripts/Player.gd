70df7fbe2c2ed57eb1b270b721c6a060984243e4
loliverbananen13369 <guanlong.zhou@minervagymnasium.se>
2022-12-09 17:48:25 +0100

    Fixade movement på olika sätt, köpte acid-animation

--- Nytt Spelprojekt/Scripts/Player.gd
+++ Nytt Spelprojekt/Scripts/Player.gd
@@ -1,25 +1,27 @@
 extends KinematicBody2D
 
+enum {IDLE, RUN, AIR, DASH, STOP}
 
-enum {IDLE, RUN, STOP, DASH}
-
-const MAX_SPEED = 200
-const ACCELERATION = 500
+const MAX_SPEED = 300
+const ACCELERATION = 1500
 const GRAVITY = 1000
-const JUMP_STRENGTH = -410
-const DASH_STRENGTH = 400
+const JUMP_STRENGHT = -410
 
 var direction_x = "RIGHT"
 var velocity := Vector2.ZERO
 var direction := Vector2.ZERO
 
 var state = IDLE
+var ghosttime := 0.0
 
-var can_jump = true
-var can_dash = true
+var can_jump := true
+var can_dash := true
+
 
 onready var animatedsprite = $AnimatedSprite
+onready var coyotetimer = $CoyoteTimer
 onready var dashtimer = $DashTimer
+
 
 func _physics_process(delta: float) -> void:
 	match state:
@@ -27,12 +29,14 @@ func _physics_process(delta: float) -> void:
 			_idle_state(delta)
 		RUN:
 			_run_state(delta)
+		AIR:
+			_air_state(delta)
 		DASH:
 			_dash_state(delta)
-		STOP: 
+		STOP:
 			_stop_state(delta)
 
-
+#Help functions
 func _apply_basic_movement(delta) -> void:
 	if direction.x != 0:
 		velocity = velocity.move_toward(direction*MAX_SPEED, ACCELERATION*delta)
@@ -52,71 +56,164 @@ func _get_input_x_update_direction() -> float:
 	return input_x
 
 
+func _air_movement(delta) -> void:
+	velocity.y = velocity.y + GRAVITY * delta if velocity.y + GRAVITY * delta < 500 else 500 
+	direction.x = _get_input_x_update_direction()
+	if direction.x != 0:
+		velocity.x = move_toward(velocity.x, direction.x * MAX_SPEED, ACCELERATION*delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, ACCELERATION * delta)
+	velocity = move_and_slide(velocity, Vector2.UP)
 
+
+#STATES:
 func _idle_state(delta) -> void:
 	direction.x = _get_input_x_update_direction()
+	if Input.is_action_just_pressed("Jump") and can_jump:
+		_enter_air_state(true)
+		return
+	
+	if Input.is_action_just_pressed("Dash") and can_dash:
+		_enter_dash_state()
+		return
 		
 	_apply_basic_movement(delta)
 	
-	if velocity.x != 0:
-		state = RUN
-		animatedsprite.play("Run")
-		can_dash = true
+	if not is_on_floor():
+		_enter_air_state(false)
 		return
-	
+	if velocity.x != 0:
+		_enter_run_state()
+		return
 		
 func _run_state(delta) -> void:
 	direction.x = _get_input_x_update_direction()
 	var input_x = Input.get_axis("move_left", "move_right")
-	_apply_basic_movement(delta)
+
+	if Input.is_action_just_pressed("Jump") and can_jump:
+		_enter_air_state(true)
+		return
 	
 	if Input.is_action_just_pressed("Dash") and can_dash:
-		can_dash = false
-		$DashTimer.start()
-		if direction.x > 0:
-			velocity.x = DASH_STRENGTH
-		elif direction.x < 0:
-			velocity.x = -DASH_STRENGTH
-		state = DASH
-		animatedsprite.play("Dash")
+		_enter_dash_state()
+		return
+		
+	if (input_x == 1 and velocity.x < 0) or (input_x == -1 and velocity.x > 0):
+		_enter_stop_state()
 		return
 	
-	elif (input_x == 1 and velocity.x <= 0) or (input_x == -1 and velocity.x >= 0):
-		state = STOP
-		animatedsprite.play("Stop")
+	_apply_basic_movement(delta)
+	
+	if not is_on_floor():
+		_enter_air_state(false)
 		return
 	elif velocity.length() == 0:
-		state = IDLE
-		animatedsprite.play("Idle")
+		_enter_idle_state()
 		return
+
+
+func _air_state(delta) -> void:
+	if Input.is_action_just_pressed("Dash") and can_dash:
+		_enter_dash_state()
+		return
+	elif Input.is_action_just_pressed("Jump") and can_jump:
+		velocity.y = JUMP_STRENGHT
+		can_jump = false
+		coyotetimer.stop()
+
 	
+	_air_movement(delta)
+	var current_animation = animatedsprite.get_animation()
+	if velocity.y > 0  and not ( current_animation == "FallN" ) and ( velocity.x == 0 ):
+		animatedsprite.play("FallN")
+	elif velocity.y > 0 and not ( current_animation == "FallF" ) and ( velocity.x != 0 ):
+		animatedsprite.play("FallF")
+	if is_on_floor():
+		_enter_idle_state()
+		return
+
+func _dash_state(delta):
+	velocity = velocity.move_toward(direction*MAX_SPEED*3, ACCELERATION*delta*3)
 	
-func _stop_state(delta) -> void:
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func _stop_state(delta):
 	direction.x = _get_input_x_update_direction()
 	var input_x = Input.get_axis("move_left", "move_right")
 	_apply_basic_movement(delta)
-	
-	if (input_x == 1 and velocity.x >= 0) or (input_x == -1 and velocity.x <= 0):
-		state = RUN
-		animatedsprite.play("Run")
-		return
-	elif velocity.length() == 0:
-		state = IDLE
-		animatedsprite.play("Idle")
-		return
-	
 
-func _dash_state(delta) -> void:
-	direction.x = _get_input_x_update_direction()
+	if Input.is_action_just_pressed("Jump") and can_jump:
+		_enter_air_state(true)
+		return
+	
+	if Input.is_action_just_pressed("Dash") and can_dash:
+		_enter_dash_state()
+		return
+		
+	if (input_x == 1 and velocity.x > 0) or (input_x == -1 and velocity.x < 0):
+		_enter_run_state()
+		return
 	
 	_apply_basic_movement(delta)
 	
-	if velocity.length() == 0:
-		state = IDLE
-		animatedsprite.play("Idle")
+	if not is_on_floor():
+		_enter_air_state(false)
+		return
+	elif velocity.length() == 0:
+		_enter_idle_state()
 		return
 	
-func _on_DashTimer_timeout() -> void:
-	state = IDLE
-	yield(get_tree().create_timer(1.0), "timeout")
+	
+
+
+#SIGNALS
+func _on_DashTimer_timeout():
+	_enter_idle_state()
+	velocity = direction * MAX_SPEED
+	direction.y = 0
+	yield(get_tree().create_timer(0.5), "timeout")
 	can_dash = true
+
+func _on_CoyoteTimer_timeout():
+	can_jump = false
+
+
+#Enter states
+func _enter_idle_state() -> void:
+	state = IDLE
+	animatedsprite.play("Idle")
+	can_jump = true
+
+
+func _enter_dash_state() -> void:
+	direction = Input.get_vector("move_left", "move_right", "ui_up", "ui_down")
+	if state == IDLE and direction == Vector2.DOWN:
+		return
+	elif direction == Vector2.ZERO:
+		direction.x = 1 if direction_x == "RIGHT" else -1
+	animatedsprite.play("Dash")
+	state = DASH
+	can_dash = false
+	dashtimer.start(0.25)
+
+func _enter_air_state(jump: bool) -> void:
+	if jump:
+		velocity.y = JUMP_STRENGHT
+	#else:
+		if velocity.x == 0:
+			animatedsprite.play("JumpN")
+		else:
+			animatedsprite.play("JumpF")
+	coyotetimer.start()
+	state = AIR
+
+func _enter_run_state() -> void:
+	state = RUN
+	animatedsprite.play("Run")
+
+func _enter_stop_state() -> void:
+	state = STOP
+	animatedsprite.play("Stop")
+	
+
+
